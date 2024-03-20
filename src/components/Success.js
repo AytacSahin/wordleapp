e48826266d45cefda2c15d3ecc660a33f782390a
AytacSahin <haytacsahin@gmail.com>
2024-03-20 19:05:01 +0300

    icon updated

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { MainContext, useContext } from '../Context';
+import coin from '../assets/svg/coin.svg'
 
 const Success = () => {
 
@@ -15,7 +16,10 @@ const Success = () => {
             <div className="bg-white p-4 rounded-md shadow-lg flex flex-col items-center gap-4">
                 <p className="text-xl">"{currentWord}"</p>
                 <p className="text-xl">Tebrikler, {currRow}. denemede buldun!</p>
-                <p className="text-xl">+{(6 - currRow) * 10}🪙</p>
+                <div className='flex'>
+                    <p className="text-xl">+{(6 - currRow) * 10}</p>
+                    <img role="coin" className='w-5 ml-1 pb-1' src={coin} />
+                </div>
                 <button
                     className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 focus:outline-none"
                     onClick={handleReset}
