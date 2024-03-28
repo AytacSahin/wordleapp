208d69dcc89dd7e7418b980c43dbee332023e981
AytacSahin <haytacsahin@gmail.com>
2024-03-28 15:27:32 +0300

    game data updated to 1000 words

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -2,7 +2,7 @@ const words = [
     // A
     "ABİDE", "ABİYE", "ACEMİ", "AFAKİ", "AFİFE", "AFİŞE", "AHALİ", "AHİZE", "AİDAT", "AKİDE", "AKTİF", "ALENİ", "ALEVİ", "AMİGO", "ANEMİ", "ANTİK", "APLİK", "ARABİ", "ARAZİ", "ARİFE", "ARŞİV", "ASABİ", "ASİST", "AŞEVİ", "AŞİNA", "ATARİ", "ATİNA", "AVİZE", "AZAMİ", "AZERİ",
     // B
-    "BABAİ", "BAHİS", "BAKİR", "BARİZ", "BASİT", "BAZİK", "BEKÇİ", "BELİK", "BELKİ", "BELLİ", "BENİZ", "BESİN", "BEŞİK", "BEŞİZ", "BEYİN", "BEYİT", "BİBER", "BİBLO", "BİÇİM", "BİÇİŞ", "BİÇKİ", "BİÇME", "BİDON", "BİLEK", "BİLET", "BİLGE", "BİLGİ", "BİLİM", "BİLYE", "BİNEK", "BİRAZ", "BİREY", "BİRİM", "BİTAP", "BİTİK", "BİTİM", "BİTİŞ", "BİTKİ", "BİTLİ", "BİTME", "BİZON", "BOBİN", "BUTİK",
+    "BAHİS", "BARİZ", "BASİT", "BAZİK", "BEKÇİ", "BELİK", "BELKİ", "BELLİ", "BENİZ", "BESİN", "BEŞİK", "BEŞİZ", "BEYİN", "BEYİT", "BİBER", "BİBLO", "BİÇİM", "BİÇİŞ", "BİÇKİ", "BİÇME", "BİDON", "BİLEK", "BİLET", "BİLGE", "BİLGİ", "BİLİM", "BİLYE", "BİNEK", "BİRAZ", "BİREY", "BİRİM", "BİTAP", "BİTİK", "BİTİM", "BİTİŞ", "BİTKİ", "BİTLİ", "BİTME", "BİZON", "BOBİN", "BUTİK",
     // C
     "CAHİL", "CAMİA", "CAZİP", "CEBİR", "CEBRİ", "CELİL", "CEMİL", "CEVİZ", "CİCİM", "CİCOZ", "CİDAR", "CİDDİ", "CİĞER", "CİHAN", "CİHAZ", "CİLVE", "CİMRİ", "CİNLİ", "CİRİT", "CİSİM", "CİVAR", "ÇETİK", "ÇEKİÇ", "ÇEKİK", "ÇEKİM", "ÇEKİŞ", "ÇELİK", "ÇELİM", "ÇENGİ", "ÇERÇİ", "ÇEŞİT", "ÇETİN", "ÇEVİK", "ÇEYİZ", "ÇİÇEK", "ÇİĞDE", "ÇİLEK", "ÇİLLİ", "ÇİMEN", "ÇİNCE", "ÇİNKO", "ÇİROZ", "ÇİZER", "ÇİZGİ", "ÇİZİK", "ÇİZİM", "ÇİZİŞ", "ÇİZME",
     // D
@@ -29,28 +29,27 @@ const words = [
     "NADİM", "NADİR", "NAFİZ", "NAKDİ", "NAKİL", "NAKİT", "NANİK", "NARİN", "NASİP", "NAŞİR", "NAZİK", "NEDİM", "NEFİS", "NEHİR", "NEMLİ", "NESİL", "NEZİH", "NİCEL", "NİÇİN", "NİĞDE", "NİHAİ", "NİKAH", "NİKEL", "NİMET", "NİNNİ", "NİSAN", "NİSPİ", "NİŞAN", "NİYAZ", "NİYET", "NİZAM",
     // O
     "OKAPİ", "OKSİT", "OPTİK", "OTİZM",
-    // Ö
-    "ÖNERİ", "ÖTEKİ",
+    // Ö ➕
+    "ÖBÜRÜ", "ÖDEME", "ÖDLEK", "ÖDÜNÇ", "ÖĞLEN", "ÖKSÜZ", "ÖLÇEK", "ÖLÇÜM", "ÖLÇÜT", "ÖNDER", "ÖNERİ", "ÖNLEM", "ÖNLÜK", "ÖRDEK", "ÖRGÜN", "ÖRGÜT", "ÖRNEK", "ÖTEKİ", "ÖTMEK", "ÖTÜCÜ", "ÖTÜRÜ", "ÖVMEK", "ÖZBEK", "ÖZERK", "ÖZGÜL", "ÖZGÜN", "ÖZGÜR", "ÖZLEM", "ÖZNEL",
     // P
     "PANİK", "PARİS", "PARTİ", "PASİF", "PATİK", "PEŞİN", "PİKAP", "PİLAV", "PİLİÇ", "PİLOT", "PİNTİ", "PİPET", "PİŞİK", "PİŞTİ", "PİTON", "PİYAZ", "PİYES", "PİYON", "PİZZA", "POLİS",
     // R
     "RAHİM", "RAHİP", "RAKİP", "RALLİ", "REHİN", "REJİM", "RESİM", "RESMİ", "REŞİT", "REVİR", "REZİL", "RİMEL", "RİNGA", "RİTİM", "RİYAL", "RUBAİ", "RUTİN", "SABİT",
     // S
     "SAHİL", "SAHİP", "SAHNE", "SAKİN", "SALİM", "SAPIK", "SANKİ", "SEBİL", "SEÇİM", "SEDİR", "SEFİL", "SEKİZ", "SELİM", "SELVİ", "SEMAİ", "SEMİH", "SEMİZ", "SERGİ", "SERİN", "SESLİ", "SEVGİ", "SEYİR", "SEYİS", "SEZGİ", "SEZİŞ", "SIHHİ", "SİCİL", "SİCİM", "SİDİK", "SİFON", "SİĞİL", "SİHİR", "SİİRT", "SİKKE", "SİLAH", "SİLGİ", "SİLİK", "SİLME", "SİMGE", "SİMİT", "SİMYA", "SİNEK", "SİNİR", "SİNME", "SİNOP", "SİNSİ", "SİNÜS", "SİPER", "SİPSİ", "SİREN", "SİRKE", "SİROZ", "SİSLİ", "SİTEM", "SİVAS", "SİVİL", "SİVRİ", "SİYAH", "SOSİS", "SÜNNİ",
-    // Ş
-    "ŞAHİN", "ŞAHİT", "ŞAHSİ", "ŞAİBE", "ŞEHİR", "ŞEHİT", "ŞEKİL", "ŞERİF", "ŞERİT", "ŞİFRE", "ŞİKAR", "ŞİLTE", "ŞİMDİ", "ŞİRİN", "ŞİŞİK", "ŞİŞKO", "ŞİŞLİ", "ŞİŞME",
-    // T
+    // Ş ➕
+    "ŞAFAK", "ŞAHİN", "ŞAHIS", "ŞAHİT", "ŞAHSİ", "ŞANLI", "ŞAPKA", "ŞARAP", "ŞARKI", "ŞAYET", "ŞEHİR", "ŞEHİT", "ŞEKER", "ŞEKİL", "ŞEREF", "ŞERİF", "ŞİFRE", "ŞİLTE", "ŞİMDİ", "ŞINAV", "ŞİRİN", "ŞİŞKO", "ŞOFÖR", "ŞÖLEN", "ŞOPAR", "ŞUBAT",
+    // T 
     "TABİİ", "TABİP", "TABİR", "TACİR", "TAHİN", "TAKİP", "TAKSİ", "TALİH", "TALİM", "TALİP", "TAMİR", "TARİF", "TARİH", "TATİL", "TAVİZ", "TAYİN", "TEKİL", "TEKİN", "TEKİR", "TELİF", "TEMİN", "TEMİZ", "TENİS", "TEORİ", "TEPKİ", "TEPSİ", "TERFİ", "TERİM", "TERLİ", "TERZİ", "TESİR", "TESİS", "TESTİ", "TETİK", "TEYİT", "TIBBİ", "TİLKİ", "TİNER", "TİPİK", "TİTAN", "TİTİZ", "TONİK", "TOPİK", "TORİK", "TRİKO",
     // U ➕
     "UCUBE", "UĞRAK", "UĞRAŞ", "UĞRUN", "UKALA", "UMMAN", "UMUMİ", "UNSUR", "USANÇ", "UŞKUN", "UTANÇ", "UYARI", "UYGAR", "UYGUN", "UYGUR", "UYRUK", "UYSAL", "UYUMA", "UZMAN",
     // Ü ➕
     "ÜÇGEN", "ÜCRET", "ÜLSER", "ÜNİTE", "ÜNLEM", "ÜNVAN", "ÜRKEK", "ÜSLUP", "ÜSTAT", "ÜSTÜN", "ÜZERİ", "ÜZGÜN", "ÜZMEK",
-    // V
-    "VACİP", "VAHİM", "VAHİY", "VAHŞİ", "VAKİT", "VALİZ", "VARİL", "VARİS", "VEKİL", "VERGİ", "VERİM", "VEZİR", "VİDEO", "VİLLA", "VİRAJ", "VİRAL", "VİRÜS", "VİSKİ", "VİŞNE", "VİTES",
-    // Y
-    "YAHNİ", "YAHŞİ", "YASİN", "YEMİN", "YEMİŞ", "YENİK", "YERLİ", "YEŞİL", "YEŞİM", "YETİK", "YETİM", "YETKİ", "YİĞİT", "YİRMİ", "YİTİK", "YİTİM",
-    // Z
-    "ZALİM", "ZAMİR", "ZARİF", "ZEHİR", "ZEMİN", "ZİGON", "ZİHİN", "ZİKİR", "ZİLLİ", "ZİRAİ", "ZİRVE", "ZİYAN",
-];
+    // V ➕
+    "VAGON", "VAHİY", "VAHŞİ", "VAKIF", "VAKİT", "VAKUM", "VALİZ", "VAPUR", "VARİL", "VARİS", "VAROŞ", "VAŞAK", "VASAT", "VASIF", "VATAN", "VATKA", "VATOZ", "VEBAL", "VEFAT", "VEGAN", "VEKİL", "VELET", "VENÜS", "VEREM", "VERGİ", "VERİM", "VEZİR", "VEZNE", "VİDEO", "VİLLA", "VİRAJ", "VİRAL", "VİRAN", "VİRÜS", "VİSKİ", "VİŞNE", "VİTES", "VOKAL", "VOLTA", "VÜCUT",
+    // Y ➕
+    "YABAN", "YAĞIŞ", "YAĞIZ", "YAĞMA", "YAHNİ", "YAHŞİ", "YAHUT", "YAKIN", "YAKIT", "YAKUT", "YALAN", "YALIN", "YALIZ", "YAMAÇ", "YAMAN", "YAMUK", "YANAK", "YANAL", "YANCI", "YANIK", "YANIT", "YANKI", "YANMA", "YANSI", "YAPAY", "YAPIM", "YAPIT", "YARAR", "YAREN", "YARGI", "YARIK", "YARIN", "YARIŞ", "YASAK", "YASAL", "YAŞAM", "YASİN", "YAŞIT", "YAŞLI", "YASSI", "YATAK", "YATAY", "YATIK", "YATIR", "YATSI", "YAVAN", "YAVAŞ", "YAVER", "YAVRU", "YAVUZ", "YAYIN", "YAYLA", "YAZAR", "YAZIK", "YAZIM", "YAZIN", "YAZIŞ", "YAZIT", "YEDEK", "YEĞEN", "YELEK", "YEMEK", "YEMEN", "YEMİN", "YEMİŞ", "YENGE", "YENİK", "YEREL", "YERLİ", "YEŞİL", "YEŞİM", "YETER", "YETİM", "YETİŞ", "YETKİ", "YIĞIN", "YİĞİT", "YIKIK", "YIKIM", "YIKMA", "YILAN", "YİRMİ", "YİTİK", "YİTİM", "YOBAZ", "YOĞUN", "YOKUŞ", "YOLCU", "YONCA", "YONGA", "YORMA", "YÖRÜK", "YORUM", "YOSUN", "YUDUM", "YUFKA", "YULAF", "YUMAK", "YUMRU", "YUMUK", "YUNAN", "YÜNLÜ", "YUNUS", "YÜREK", "YUTAK", "YÜZDE", "YÜZEY", "YÜZME", "YÜZÜK",
+    // Z ➕
+    "ZAFER", "ZALİM", "ZAMAN", "ZAMİR", "ZANLI", "ZARAR", "ZARİF", "ZATEN", "ZAYIF", "ZEBRA", "ZEBUR", "ZEHİR", "ZEKAT", "ZEMİN", "ZENCİ", "ZENNE", "ZEVAL", "ZİFİR", "ZİGON", "ZİHİN", "ZİKİR", "ZIMBA", "ZİNDE", "ZIPIR", "ZİRVE", "ZİYAN", "ZOMBİ", "ZORBA", "ZORLA", "ZORLU", "ZÜHAL", "ZÜLÜF", "ZULÜM", "ZÜPPE", "ZURNA",];
 export { words };
 
